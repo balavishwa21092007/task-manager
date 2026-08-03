c841dc2bac98675c7e6907ed199bc92be410c22c
Bala <balavishwa21092007@gmail.com>
2026-08-03 15:50:10 +0530

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://task-manager-one-rosy-73.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
